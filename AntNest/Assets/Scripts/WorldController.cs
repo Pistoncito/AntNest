e3aa30b26521d5dc76c39ef2f9aa4562297a0369
Adrián Poza <adripoza23@gmail.com>
2019-10-23 23:32:16 +0200

    primer acercamiento

---
+++ AntNest/Assets/Scripts/WorldController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldController : MonoBehaviour
+{
+    //Editor variables
+    public Ant antmolde;
+
+    //Game Object lists
+    List<Ant> ants = new List<Ant>();
+
+    //Game parameters
+    float temperature;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
